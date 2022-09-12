2980253e614de7307b0228d145c50b31733bae6c
Vinícius Polletti <viniciuspolletti2015@gmail.com>
2022-09-12 17:14:17 -0300

    Configurando a classe pessoa

--- src/br/com/residencia/poo/sistema_bancario_principal/pessoas/Pessoa.java
+++ src/br/com/residencia/poo/sistema_bancario_principal/pessoas/Pessoa.java
@@ -1,5 +1,33 @@
 package br.com.residencia.poo.sistema_bancario_principal.pessoas;
 
-public class Pessoa {
+public abstract class Pessoa {
+
+	private String senha;
+	private String cpf;
+	private char tipoPessoa;
+
+	public Pessoa() {
+		super();
+
+	}
+
+	public Pessoa(String senha, String cpf, char tipoPessoa) {
+		super();
+		this.senha = senha;
+		this.cpf = cpf;
+		this.tipoPessoa = tipoPessoa;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public char getTipoPessoa() {
+		return tipoPessoa;
+	}
 
 }
